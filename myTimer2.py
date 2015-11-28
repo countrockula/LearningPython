d331401eacef18e13255b67c64f9ef2e62a0fdae
countrockula <thecounthimself@gmail.com>
2015-11-28 04:09:09 -0500

    Revised timer, meant for side branch

--- myTimer2.py
+++ myTimer2.py
@@ -2,11 +2,8 @@ from datetime import datetime
 import time
 
 
-response_1 = raw_input("Start timer now?").lower()
-divider = "-" * 80
-
-
 # starts timer
+
 def start_timer():
 
 	global start_time
@@ -31,18 +28,45 @@ def stop_timer():
 	log_name = ((time.strftime("%d_%m_%y")) + ".log")
 	update_log = open(log_name , "a")
 	update_log.write(total_time_duration + "\n")
-
 	update_log.close()
 
-	
+# Adds up log times and prints it out in total minutes
 
+def add_total():
+	print divider + "\n"
+	log_name = raw_input("Please enter in the name of log file: ")
+	add_log = open(log_name, "r")
+	time_list = add_log.readlines()
+	add_log.close()
+
+	total_minutes = 0
+	for i in time_list:
+		hours = int(i[0]) * 60
+		minutes = int(i[2:4])
+		seconds = float(i[5:7]) / 60
+		total_minutes += hours + minutes + seconds
+
+	print  ("%.2f minutes total" % total_minutes)
+	
+# Start of program
+
+# Asks user if timer should start
+response_1 = raw_input("Start timer now?").lower()
+divider = "-" * 80
 while (response_1 == "yes" or response_1 == "y"):
 	start_timer()
 	stop_timer()
 	response_1 = raw_input("Would you like to run timer again?").lower()
 
-print divider + "\n"
+# Asks user if they would like a log file totaled
+response_2 = raw_input("Would you like to get a sum total of a file?").lower()
+while (response_2 == "yes" or response_2 == "y"):
+	add_total()
+	response_2 = raw_input("Would you like to get a sum total of a file?").lower()
 
+# End of program
+print divider + "\n"
 print "Alright, goodbye\n"
+print divider + "\n"
 	
 
